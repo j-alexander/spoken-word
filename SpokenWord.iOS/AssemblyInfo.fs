612dc5a127cb1024ac47a16df324919928dbfde0
Jonathan Leaver <jonathan@jet.com>
2016-09-17 17:27:35 -0400

    include attribution information

--- SpokenWord.iOS/AssemblyInfo.fs
+++ SpokenWord.iOS/AssemblyInfo.fs
@@ -1,6 +1,16 @@
 ﻿namespace SpokenWord.AssemblyInfo
 
+open System.Resources
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("SpokenWord.iOS")>]
+[<assembly: AssemblyCompany("lambda-fun.com")>]
+[<assembly: AssemblyProduct("SpokenWord")>]
+[<assembly: AssemblyCopyright("Copyright ©2016 Jonathan Leaver")>]
+[<assembly: NeutralResourcesLanguage("en")>]
+
 [<assembly: Xamarin.Forms.Dependency(typeof<SpokenWord.SiriSpeech>)>]
 
-do
-    ()
+do ()
